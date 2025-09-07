bf5b92ea4f8f7609094d6f3bef850150a2d7f7ca
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-07 11:22:17 +0530

    Add Authentication component for route-based token validation and navigation 🔒

---
+++ src/components/Authentication.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { getTokens } from "../utils/helpers/warrperRouter.ts";
+
+const Authentication = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { isTokenAvailable } = getTokens();
+
+  useEffect(() => {
+
+    if (!isTokenAvailable && pathname !== "/login" && pathname !== "/signup") {
+      navigate("/login", { replace: true });
+      return;
+    }
+
+    if (isTokenAvailable && (pathname === "/login" || pathname === "/signup" || pathname === "/")) {
+      navigate("/home", { replace: true });
+    }
+  }, [isTokenAvailable, pathname, navigate]);
+
+  return <Outlet />;
+};
+
+export default Authentication;
