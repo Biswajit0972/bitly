f156203a4167b99d4da4c8cf0b80d0420e78c6ed
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-07 10:48:15 +0530

    Add router setup with routes for authentication and error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import AuthLayout from "./pages/Layouts/AuthLayout.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Login from "./pages/login.tsx";
+import Signup from "./pages/Signup.tsx";
+import NotFound from "./pages/Error.tsx";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: AuthLayout,
+        children: [
+            {
+                path: "login",
+                Component: Login
+            },
+            {
+                path: "signup",
+                Component: Signup
+            },
+            {index: true, Component: Login},
+            {
+                path: "*",
+                Component: NotFound
+            }
+        ]
+    },
 
+])
 const App = () => {
     return (
-        <AuthLayout/>
+        <RouterProvider router={router}/>
     )
 }
 export default App
