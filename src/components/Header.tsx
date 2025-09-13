4b52feb8764fba6ba70f6fbd7a39adb1b458a673
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-13 09:49:58 +0530

    Add login state handling in `Header` with conditional rendering for login/logout actions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,21 @@ import {Link} from "react-router-dom";
 import {Link2} from "lucide-react";
 import {basicNavigationLink} from "../utils";
 import List from "./List.tsx";
+import {useEffect, useState} from "react";
+import {getTokens} from "../utils/helpers/warrperRouter.ts";
+import {Button} from "./Button.tsx";
 
 const Header = () => {
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
+    useEffect(() => {
+    const {isTokenAvailable} = getTokens();
+        if (isTokenAvailable) {
+            setLoggedIn(true);
+        } else {
+            setLoggedIn(false);
+        }
+    }, [loggedIn]);
+
     return (
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between ">
@@ -26,16 +39,21 @@ const Header = () => {
                           </li>}/>
                 </nav>
 
-                <div className="flex items-center gap-3">
-                    <Link to="/auth/login"
-                          className="hidden rounded-md px-3 py-2 text-sm text-white/80 transition-colors hover:text-white md:inline-block">
-                        Log in
-                    </Link>
-                    <Link to="/auth/signup"
-                          className="rounded-md bg-white px-3.5 py-2 text-sm font-medium text-black transition-colors hover:bg-white/90">
-                        Sign up
-                    </Link>
-                </div>
+                {
+                    !loggedIn ? (<div className="flex items-center gap-3">
+                        <Link to="/auth/login"
+                              className="hidden rounded-md px-3 py-2 text-sm text-white/80 transition-colors hover:text-white md:inline-block">
+                            Log in
+                        </Link>
+                        <Link to="/auth/signup"
+                              className="rounded-md bg-white px-3.5 py-2 text-sm font-medium text-black transition-colors hover:bg-white/90">
+                            Sign up
+                        </Link>
+                    </div>) : <Button variant="destructive" onClick={() => {
+                        localStorage.removeItem("Token");
+                        setLoggedIn(false);
+                    }}>Logout</Button>
+                }
             </div>
         </div>
     )
