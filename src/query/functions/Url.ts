6f234cc4a9aeb44ac413b0e6dd8a1243720ed15e
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-14 23:26:24 +0530

    Add `getAllShortUrls` and `deleteShortUrl` functions for URL management API integration

--- src/query/functions/Url.ts
+++ src/query/functions/Url.ts
@@ -15,3 +15,32 @@ export const getShortUrl = ErrorWrapper(async (data: shortUrlType) => {
     });
     return response.data;
 })
+
+export const getAllShortUrls = ErrorWrapper(async () => {
+    if (!token) {
+        throw new Error("Token not found");
+    }
+
+    const response = await axios.get("http://localhost:3000/api/urls/all", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        withCredentials: true,
+    });
+    console.log(response.data)
+
+    return response.data;
+});
+
+export const deleteShortUrl = ErrorWrapper(async (id: string) => {
+    if (!token) {
+        throw new Error("Token not found");
+    }
+    const response = await axios.delete(`http://localhost:3000/api/urls/delete/${id}`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        withCredentials: true,
+    });
+    return response.data;
+});
