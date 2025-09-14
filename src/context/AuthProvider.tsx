30b062bbc76887c3668c6af6152297d068440198
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-15 00:09:53 +0530

    Integrate `react-query` for token revalidation and error handling in `AuthProvider`.

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,13 +1,31 @@
 import {AuthContext} from "./Auth.ts";
 import {type ReactNode, useEffect, useState} from "react";
 import {getTokens} from "../utils/helpers/warrperRouter.ts";
+import {revalidateToken} from "../query/functions/authentication.ts";
+import {useQuery} from "@tanstack/react-query";
+import {toast} from "react-toastify";
 
 const AuthProvider = ({children}: { children: ReactNode }) => {
+
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+    const {isPending, data, isError, error} = useQuery({
+        queryKey: ["revalidateToken"],
+        queryFn: revalidateToken,
+        enabled: isLoggedIn,
+    });
+
+    if (isError) {
+        console.log(error)
+        toast.error(error?.message)
+    }
+
+    if (!isPending) {
+        console.log(data)
+    }
+
     useEffect(() => {
         const {isTokenAvailable} = getTokens();
-        console.log(isTokenAvailable)
-
         setIsLoggedIn(isTokenAvailable);
     }, []);
 
@@ -18,7 +36,8 @@ const AuthProvider = ({children}: { children: ReactNode }) => {
         } else {
             setIsLoggedIn(false);
         }
-    }, [isLoggedIn])
+    }, [isLoggedIn]);
+
     return (
         <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>{
             children
