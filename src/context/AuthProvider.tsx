c11969a8ed0c6e5e87db128fd9fcfe1820164520
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-24 02:23:00 +0530

    Refactor `AuthProvider` to simplify state management by removing `useQuery` for token revalidation and redundant `useEffect` hooks.

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,42 +1,9 @@
 import {AuthContext} from "./Auth.ts";
-import {type ReactNode, useEffect, useState} from "react";
-import {getTokens} from "../utils/helpers/warrperRouter.ts";
-import {revalidateToken} from "../query/functions/authentication.ts";
-import {useQuery} from "@tanstack/react-query";
-import {toast} from "react-toastify";
+import {type ReactNode, useState} from "react";
+
 
 const AuthProvider = ({children}: { children: ReactNode }) => {
-
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
-    const {isPending, data, isError, error} = useQuery({
-        queryKey: ["revalidateToken"],
-        queryFn: revalidateToken,
-        enabled: isLoggedIn,
-    });
-
-    if (isError) {
-        console.log(error)
-        toast.error(error?.message)
-    }
-
-    if (!isPending) {
-        console.log(data)
-    }
-
-    useEffect(() => {
-        const {isTokenAvailable} = getTokens();
-        setIsLoggedIn(isTokenAvailable);
-    }, []);
-
-    useEffect(() => {
-        const {isTokenAvailable} = getTokens();
-        if (isTokenAvailable) {
-            setIsLoggedIn(isTokenAvailable);
-        } else {
-            setIsLoggedIn(false);
-        }
-    }, [isLoggedIn]);
 
     return (
         <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>{
